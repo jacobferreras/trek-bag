d884861df01179c60ce4230a08eec6efb6f19e57
Rhobel Jacob S. Ferreras <152048667+jacobferreras@users.noreply.github.com>
2025-09-08 18:02:07 +0800

    Add value attributes to options in FilterButton component for improved accessibility

--- components/FilterButton.tsx
+++ components/FilterButton.tsx
@@ -4,9 +4,9 @@ const FilterButton = () => {
   return (
     <>
       <select className="select select-bordered w-100 max-w-lg bg-[#ffffff] border-2 border-gray-400 text-black ">
-        <option>Sort By Default</option>
-        <option>Sort By Packed</option>
-        <option>Sort By Unpacked</option>
+        <option value="">Sort By Default</option>
+        <option value="packed">Sort By Packed</option>
+        <option value="unpacked">Sort By Unpacked</option>
       </select>
     </>
   );
