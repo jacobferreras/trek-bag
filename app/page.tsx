cb394f6fe727347489552c7fc688fc9cb1bd8615
Rhobel Jacob S. Ferreras <152048667+jacobferreras@users.noreply.github.com>
2025-09-10 11:31:40 +0800

    Refactor Container and Home components to improve layout structure and maintain consistent styling

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <div className="bg-[#f0d3b3] min-h-screen flex flex-col items-center justify-center">
-        <Container />
-      </div>
+      <Container />
     </>
   );
 }
--- components/Container.tsx
+++ components/Container.tsx
@@ -6,11 +6,13 @@ import Sidebar from "./Sidebar";
 const Container = () => {
   return (
     <>
-      <div className="card w-auto bg-[#ffffff] card-xl shadow-sm">
-        <HeaderCount />
-        <div className="card-body flex flex-row justify-between gap-4 overflow-y-scroll h-[80vh]">
-          <List />
-          <Sidebar />
+      <div className="bg-[#f0d3b3] min-h-screen flex flex-col items-center justify-center">
+        <div className="card w-auto bg-[#ffffff] card-xl shadow-sm">
+          <HeaderCount />
+          <div className="card-body flex flex-row justify-between gap-4 overflow-y-scroll h-[80vh]">
+            <List />
+            <Sidebar />
+          </div>
         </div>
       </div>
     </>
