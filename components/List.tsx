cc09ee505991fd0a4e53d8544668d0a187cb05b0
Rhobel Jacob S. Ferreras <152048667+jacobferreras@users.noreply.github.com>
2025-09-10 10:51:38 +0800

    Refactor List component to utilize useSortItems hook and improve item rendering

--- components/List.tsx
+++ components/List.tsx
@@ -1,38 +1,41 @@
-import React from "react";
+"use client";
+import { useSortItems } from "@/hooks/useSortItems";
 import { ImCross } from "react-icons/im";
-interface ListProps {
-  item: string;
-  isCompleted: boolean;
-  handleToggleItems: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  onDeleteItem?: () => void;
-}
 
-const List = ({
-  item,
-  isCompleted,
-  handleToggleItems,
-  onDeleteItem,
-}: ListProps) => {
+const List = () => {
+  const { sortItems, handleToggleItems, onDeleteItem } = useSortItems();
+
   return (
     <>
+      {sortItems.length === 0 && (
+        <h1 className="text-lg text-black text-center">No Items</h1>
+      )}
       <ul className="list bg-base-white rounded-box shadow-md text-black w-100">
-        <li className="list-row w-full flex flex-row justify-between items-center p-4">
-          <div className="flex flex-row gap-4 items-center">
-            <input
-              type="checkbox"
-              checked={isCompleted}
-              className="checkbox checkbox-neutral"
-              onChange={handleToggleItems}
-            />
+        {sortItems.map((item) => (
+          <li
+            key={item.id}
+            className="list-row w-full flex flex-row justify-between items-center p-4"
+          >
+            <div className="flex flex-row gap-4 items-center">
+              <input
+                type="checkbox"
+                checked={item.completed}
+                className="checkbox checkbox-neutral"
+                onChange={() => handleToggleItems(item.id)}
+              />
 
-            <h1 className="text-lg font-bold">{item}</h1>
-          </div>
-          <div>
-            <button className="btn btn-square btn-ghost" onClick={onDeleteItem}>
-              <ImCross />
-            </button>
-          </div>
-        </li>
+              <h1 className="text-lg font-bold">{item.text}</h1>
+            </div>
+            <div>
+              <button
+                className="btn btn-square btn-ghost"
+                onClick={() => onDeleteItem(item.id)}
+              >
+                <ImCross />
+              </button>
+            </div>
+          </li>
+        ))}
       </ul>
     </>
   );
