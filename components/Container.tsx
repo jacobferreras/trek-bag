a1e6092ffcdf83567879916ea26fbfc6af1a2e3c
Rhobel Jacob S. Ferreras <152048667+jacobferreras@users.noreply.github.com>
2025-09-10 10:51:53 +0800

    Refactor Container component to simplify state management and improve readability

--- components/Container.tsx
+++ components/Container.tsx
@@ -1,68 +1,26 @@
-"use client";
 import React from "react";
 import List from "./List";
 import AddFrom from "./AddFrom";
 import ButtonsContainer from "./ButtonsContainer";
-import { useItems } from "../hooks/useItems";
 import FilterButton from "./FilterButton";
 
-const Container = () => {
-  const {
-    sortItems,
-    setSortBy,
-    totalCompletedItems,
-    text,
-    totalItems,
-    handleChange,
-    handleAddItem,
-    handleToggleItems,
-    handleDeleteItems,
-    handleRemoveAllItems,
-    handleMarkAllAsCompleted,
-    handleMarkAllAsIncomplete,
-    inputRef,
-  } = useItems();
+import HeaderCount from "./HeaderCount";
 
+const Container = () => {
   return (
     <>
       <div className="card w-auto bg-[#ffffff] card-xl shadow-sm">
-        <h1 className="text-xl font-bold text-black text-end pt-4 pr-12">
-          {totalCompletedItems}/{totalItems} Items packed
-        </h1>
+        <HeaderCount />
         <div className="card-body flex flex-row justify-between gap-4 overflow-y-scroll h-[80vh]">
           <div className="flex flex-col gap-4 w-2/3">
-            <FilterButton
-              onChange={(e) =>
-                setSortBy(e.target.value as "all" | "packed" | "unpacked")
-              }
-            />
+            <FilterButton />
             <h1 className="text-xl text-black">Your Items</h1>
-            {sortItems.length === 0 && (
-              <h1 className="text-lg text-black text-center">No Items</h1>
-            )}
-            {sortItems.map((item) => (
-              <List
-                key={item.id}
-                item={item.text}
-                isCompleted={item.completed}
-                handleToggleItems={() => handleToggleItems(item.id)}
-                onDeleteItem={() => handleDeleteItems(item.id)}
-              />
-            ))}
+            <List />
           </div>
 
           <div className="flex flex-col gap-4 sticky top-0 z-10">
-            <AddFrom
-              inputRef={inputRef}
-              text={text}
-              onTextChange={handleChange}
-              onAddItem={handleAddItem}
-            />
-            <ButtonsContainer
-              onRemoveAllItems={handleRemoveAllItems}
-              onMarkAllAsCompleted={handleMarkAllAsCompleted}
-              onMarkAllAsIncomplete={handleMarkAllAsIncomplete}
-            />
+            <AddFrom />
+            <ButtonsContainer />
           </div>
         </div>
       </div>
