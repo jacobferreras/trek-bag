63dec31708ad8e723335271eedee209d7b4a5f04
Rhobel Jacob S. Ferreras <152048667+jacobferreras@users.noreply.github.com>
2025-09-10 11:29:08 +0800

    Refactor List component to integrate setSortBy from useSortItems and improve structure

--- components/List.tsx
+++ components/List.tsx
@@ -1,42 +1,48 @@
 "use client";
 import { useSortItems } from "@/hooks/useSortItems";
 import { ImCross } from "react-icons/im";
+import FilterButton from "./FilterButton";
 
 const List = () => {
-  const { sortItems, handleToggleItems, onDeleteItem } = useSortItems();
+  const { sortItems, setSortBy, handleToggleItems, onDeleteItem } =
+    useSortItems();
 
   return (
     <>
-      {sortItems.length === 0 && (
-        <h1 className="text-lg text-black text-center">No Items</h1>
-      )}
-      <ul className="list bg-base-white rounded-box shadow-md text-black w-100">
-        {sortItems.map((item) => (
-          <li
-            key={item.id}
-            className="list-row w-full flex flex-row justify-between items-center p-4"
-          >
-            <div className="flex flex-row gap-4 items-center">
-              <input
-                type="checkbox"
-                checked={item.completed}
-                className="checkbox checkbox-neutral"
-                onChange={() => handleToggleItems(item.id)}
-              />
+      <div className="flex flex-col gap-4 w-2/3">
+        <FilterButton setSortBy={setSortBy} />
+        <h1 className="text-lg text-black font-bold text-center">Items List</h1>
+        {sortItems.length === 0 && (
+          <h1 className="text-lg text-black text-center">No Items</h1>
+        )}
+        <ul className="list bg-base-white rounded-box shadow-md text-black w-100">
+          {sortItems.map((item) => (
+            <li
+              key={item.id}
+              className="list-row w-full flex flex-row justify-between items-center p-4"
+            >
+              <div className="flex flex-row gap-4 items-center">
+                <input
+                  type="checkbox"
+                  checked={item.completed}
+                  className="checkbox checkbox-neutral"
+                  onChange={() => handleToggleItems(item.id)}
+                />
 
-              <h1 className="text-lg font-bold">{item.text}</h1>
-            </div>
-            <div>
-              <button
-                className="btn btn-square btn-ghost"
-                onClick={() => onDeleteItem(item.id)}
-              >
-                <ImCross />
-              </button>
-            </div>
-          </li>
-        ))}
-      </ul>
+                <h1 className="text-lg font-bold">{item.text}</h1>
+              </div>
+              <div>
+                <button
+                  className="btn btn-square btn-ghost"
+                  onClick={() => onDeleteItem(item.id)}
+                >
+                  <ImCross />
+                </button>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
     </>
   );
 };
